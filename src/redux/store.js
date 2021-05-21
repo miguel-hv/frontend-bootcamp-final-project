5e5298ef2e02cea913319dd1c7c839e704a755ea
@Loretodpinedo <ororomb@yahoo.es>
2021-05-21 14:13:32 +0200

    FEAT: user slice done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { pickupsSlice } from './reducers/pickups.slice';
 import { donationsSlice } from './reducers/donations.slice';
+import { userSlice } from './reducers/users.slice';
+
 
 export default configureStore({
     reducer: {
-        pets: pickupsSlice.reducer,
+        pickups: pickupsSlice.reducer,
         donations: donationsSlice.reducer,
+        user: userSlice.reducer,
     }
 });
