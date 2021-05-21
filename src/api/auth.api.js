42c151b70f96fe22941eb07eeac9180e96a8a684
@Loretodpinedo <ororomb@yahoo.es>
2021-05-21 16:46:39 +0200

    REFACTOR: Login with redux working

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,5 +1,7 @@
 const registerUrl = "http://localhost:3500/auth/register";
+const loginUrl = "http://localhost:3500/auth/login";
 const checkSessionUrl = "http://localhost:3500/auth/check-session";
+const logoutUrl = "http://localhost:3500/auth/logout";
 
 
 export const register = async (form) => {
@@ -22,6 +24,24 @@ export const register = async (form) => {
     }
     return response;
 };
+export const login = async (form) => {
+    const request = await fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Access-Control-Allow-Origin': '*' 
+        },
+        credentials: 'include',
+        body: JSON.stringify(form),
+    });
+    const response = await request.json();
+    if (!request.ok) {
+        throw new Error(response.message);
+    }
+
+    return response;
+};
 
 export const checkSession = async (form) => {
     const request = await fetch(checkSessionUrl, {
@@ -29,7 +49,7 @@ export const checkSession = async (form) => {
         headers: {
            'Accept': 'application/json',
            'Content-Type': 'application/json',
-           'Access-Control-Allow-Origin': '*' // CORS
+           'Access-Control-Allow-Origin': '*' 
         },
         credentials: 'include',
     });
@@ -38,4 +58,18 @@ export const checkSession = async (form) => {
     return response;
 };
 
+export const logout = async () => {
+    const request = await fetch(logoutUrl, {
+        method: 'POST',
+        headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Access-Control-Allow-Origin': '*' // CORS
+        },
+        credentials: 'include',
+    });
 
+    const response = await request.json();
+
+    return response;
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -11,3 +11,4 @@ export default configureStore({
         user: userSlice.reducer,
     }
 });
+
